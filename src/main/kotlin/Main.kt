c2cc2cb24d26cf42bde43673078c51349b2f77e4
manpreet <manpreetrahal27@gmail.com>
2024-06-23 14:09:49 -0600

    config port

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,15 +5,18 @@ import org.slf4j.LoggerFactory
 
 val logger: Logger = LoggerFactory.getLogger("Main")
 
-fun main() {
+fun main(args: Array<String>) {
+    val port = args[0].toInt()
     logger.info("${ConfigMap.configMap}")
 
+
     Javalin.create { config ->
+        config.bundledPlugins.enableDevLogging()
         config.requestLogger.http { ctx, ms ->
             val req = ctx.req()
             logger.info("Request: ${req.method} ${req.requestURL} | Response: ${ctx.res().status} | Time: ${ms}ms")
         }
-    }.get("<memeName>/{textToAdd}", RequestHandler).start(80)
+    }.get("<memeName>/{textToAdd}", RequestHandler).start(port)
 
 
 }
